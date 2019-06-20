b670558dd98400a25a4521b24d932faa6217e6a0
polarnight <miku70619@gmail.com>
2019-06-20 10:08:33 +0800

    first

---
+++ slide-demo/lunbo.js
@@ -0,0 +1,40 @@
+var allButtons = $('#buttons > span')
+
+for (let i = 0; i < allButtons.length; i++) {
+  $(allButtons[i]).on('click', function (x) {
+    var index = $(x.currentTarget).index()
+    n = index
+    var p = index * -400
+    $('#image').css({
+      transform: 'translate(' + p + 'px)'
+    })
+    activeButton(allButtons.eq(n))
+  })
+}
+
+var n = 0;
+var size = allButtons.length
+allButtons.eq(n % size).trigger('click')
+
+var timerId = setTimer()
+
+function setTimer() {
+  return setInterval(() => {
+    n += 1
+    allButtons.eq(n % size).trigger('click')
+  }, 1000)
+}
+
+function activeButton($button) {
+  $button
+    .removeClass('default').addClass('active')
+    .siblings('.active').removeClass('active').addClass('default')
+}
+
+$('.window').on('mouseenter', function () {
+  window.clearInterval(timerId)
+})
+
+$('.window').on('mouseleave', function () {
+  timerId = setTimer()
+})
